683d9e747e18e3cf542d9dc9805ff7356fe3a46a
Soni Saini <103830336+sonisaini901@users.noreply.github.com>
2026-06-13 11:47:05 +0100

    commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const MONGO_URI = process.env.MONGO_URI;
+console.log(MONGO_URI);
 
-mongoose.set("strictQuery", false);
 
-const connectDatabase = () => { mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-
-}).then(() => {
-    console.log("connection successful!...");
-}).catch((err) => {
-    console.log(`connection failed!.... ${err}`);
-});
-}
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB Error:", err);
+    console.error("Reason:", err.reason);
+  }
+};
 
 module.exports = connectDatabase;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dns').setDefaultResultOrder('ipv4first');
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
